f3aacc29b88e8af227348c9d2fe0555628a17b1e
Woohyun Jo <bluewing1004@gmail.com>
2025-03-06 15:12:14 +0900

    Initial Commit

---
+++ AllLaRight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AllLaRight
+//
+//  Created by 조우현 on 3/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
