47c0982a1ea5af5a8f3f91d06c2a4883c4290f51
Woohyun Jo <bluewing1004@gmail.com>
2025-03-06 15:43:13 +0900

    [Setting] 앱 기본 세팅 폴더링 및 gitignore 세팅

--- AllLaRight/View/Market/MarketViewController.swift
+++ AllLaRight/View/Market/MarketViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MarketViewController.swift
 //  AllLaRight
 //
 //  Created by 조우현 on 3/6/25.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MarketViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .brown
     }
-
-
 }
 
